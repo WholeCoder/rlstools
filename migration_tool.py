2b663a0b4cd684c0d7e022e374e005368f71cee2
Ruben Pierich <rpierich@hotmail.com>
2019-06-16 18:06:55 -0400

    Added so migrations work correctly.

--- migration_tool.py
+++ migration_tool.py
@@ -10,17 +10,24 @@ def doesColoumnExist(conn,table,column):
     cur = conn.cursor()
     columns = [i[1] for i in cur.execute('PRAGMA table_info('+table+')')]
     column = column.split(":")[0].strip()
-
-    if column not in columns: 
+    
+    print("doesColumnExist(conn,"+table+","+column+")")
+    if column in columns: 
         print("\tColumn "+column+" in table " + table + " does not exist")
+        print("column returned " + str(column not in columns))
         return True
     else:
         print("\tColumn " + column + " already exists on table " + table)
+        print("column returned " + str(column not in columns))
         return False
 
 def deleteColumnFromTable(conn, table, column):
+    print("i--------------------------inside deleteColumnFromTable function")
     if doesColoumnExist(conn,table,column): 
         cur = conn.cursor()
+        dropBackupTableStatement = "drop table t1_backup"
+        cur.execute(dropBackupTableStatement)
+
         columns = [i[1] for i in cur.execute('PRAGMA table_info('+table+')')]
 
         selectClause = " "
@@ -28,11 +35,17 @@ def deleteColumnFromTable(conn, table, column):
             if columnInColumns != column:
                 selectClause = columnInColumns + ","
         selectClause = selectClause[:len(selectClause)-1]
-        c.exicute("CREATE TABLE t1_backup AS SELECT "+selectClause+" FROM "+table)
+        selectStatement ="CREATE TABLE t1_backup AS SELECT "+selectClause+" FROM "+table  
+        print("\n\t"+selectStatement)
+        cur.execute(selectStatement)
         # CREATE TABLE t1_backup AS SELECT a, b FROM t1;
-        c.execute("drop "+table)
+        dropTableStatement = "drop table "+table
+        print("\n\t" + dropTableStatement)
+        cur.execute(dropTableStatement)
         # DROP TABLE t1;
-        c.execute("ALTER TABLE t1_backup RENAME TO " + table) 
+        alterTableStatement = "ALTER TABLE t1_backup RENAME TO " + table
+        print("\n\t"+ alterTableStatement)
+        cur.execute(alterTableStatement) 
         # ALTER TABLE t1_backup RENAME TO t1;
 
 #"   SELECT count(name) FROM sqlite_master WHERE type='table' AND name='"+table+"'"
@@ -55,10 +68,16 @@ file_contents = file_handle.readlines()
 for i in range(len(file_contents)):
     if file_contents[i].strip() != '\n':
         split_line = file_contents[i].split(' ')
+        if split_line[0].strip().startswith("#"):
+            i = i + 1
+            continue
         if split_line[0].strip() == "upgrade":
             print(split_line[0].strip())
             i = i+1
             while i in range(len(file_contents)) and file_contents[i].strip() != "" and file_contents[i] != 'downgrade':
+                if file_contents[i].strip().startswith("#"):
+                    i = i + 1
+                    continue
                 if file_contents[i].split(" ")[1] == "table":
                     print("adding table "+file_contents[i].split(" ")[2].strip())
                     c = conn.cursor()
@@ -70,28 +89,34 @@ for i in range(len(file_contents)):
                         c.execute(createTableString)
                 elif file_contents[i].split(" ")[1] == "column":
                     #print("\tadding column "+file_contents[i].split(" ")[3].strip())
-                    if doesColoumnExist(conn,file_contents[i].split(" ")[2],file_contents[i].split(" ")[3].strip()):
+                    if not doesColoumnExist(conn,file_contents[i].split(" ")[2],file_contents[i].split(" ")[3].strip()):
                         c.execute('ALTER TABLE '+file_contents[i].split(" ")[2]+' ADD COLUMN '+file_contents[i].split(" ")[3].split(':')[0]+' TEXT')
  
                 i = i+1
-
+        
         split_line = file_contents[i].split(' ')
         if split_line[0].strip() == "downgrade":
             print(split_line[0].strip())
             i = i+1
             while i in range(len(file_contents)) and file_contents[i].strip() != "" and file_contents[i] != '':
+                if file_contents[i].strip().startswith("#"):
+                    i = i + 1
+                    continue
                 if file_contents[i].split(" ")[1] == "table":
-                    if  doesTableExist(conn,file_contents[i].split(" ")[2]):
+                    if doesTableExist(conn,file_contents[i].split(" ")[2]):
 
                         print("removing table "+file_contents[i].split(" ")[2].strip())
+                        # deleteColumnFromTable(conn, file_contents[i].split(" ")[2].strip(),file_contents[i].split(" ")[3].strip()) 
                         c.execute("DROP TABLE "+file_contents[i].split(" ")[2])
                 elif file_contents[i].split(" ")[1] == "column":
-                    if doesColoumnExist(conn,file_contents[i].split(" ")[2],file_contents[i].split(" ")[3].strip()):
-
-                        c.execute()
+                    print(" checking to removed == "+file_contents[i].split(" ")[2]+", "+file_contents[i].split(" ")[3].strip())
+                    print(doesColoumnExist(conn,file_contents[i].split(" ")[2].strip(),file_contents[i].split(" ")[3].strip()))
+                    if doesColoumnExist(conn,file_contents[i].split(" ")[2].strip(),file_contents[i].split(" ")[3].strip()):
+                       
+                        deleteColumnFromTable(conn, file_contents[i].split(" ")[2],file_contents[i].split(" ")[3].strip())
+                        
                         print("\tremoving column "+file_contents[i].split(" ")[3].strip())
                 i = i+1
-
+    i = i + 1
 conn.commit()
-conn.close()
- 
+
