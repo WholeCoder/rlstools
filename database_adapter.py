f2f46108dc3bdb347ccbfdf02d8c0c023d8d631d
Ruben Pierich <rpierich@hotmail.com>
2019-06-17 23:05:31 -0400

    Added new adapter class for database.

---
+++ database_adapter.py
@@ -0,0 +1,26 @@
+class DatabaseAdapter:
+    def __init__(self):
+        print("Initializing adapter")
+
+    def doesColumnExist(self,table, column):
+        print("Seeing if column " + column + " exists on table " + table)
+
+    def deleteColumnFromTable(self,table, column):
+        print("Deleting column " + column + " from table "+table)
+
+    def doesTableExist(self,table):
+        print("Seeing if table "+ table +" exists."
+
+    def createTable(self,table):
+        print("Creating table "+table)
+
+    def addColumn(self,table, column):
+        print("Added column "+column+" to table "+ table)
+
+    def removeTable(self,table):
+        print("Removing table "+ table)
+
+
+
+adapter = DatabaseAdapter()
+
