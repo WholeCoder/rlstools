5071bbaab4058a58bcc1e72e43ed22af219a324c
Ruben Pierich <rpierich@hotmail.com>
2019-07-12 08:24:47 -0400

    Removed *.sh~ file.

--- template_output.py
+++ template_output.py
@@ -1,17 +1,17 @@
-from rubsapp.models.Person import Person
+from rubsapp.models.Office import Office
 
 print("Running template_Output")
 
-rows = Person().findAll()
+rows = Office().findAll()
 print("rows == "+str(rows))
 
 currString = ""
-currString +="<html><head><title>Ruben'sPeople</title></head><body><table><tbody><tr><th>last_name</th><th>first_name</th></tr>"
+currString +="<html><head><title>Office</title></head><body><table><tbody><tr><th>zip_code</th><th>city</th></tr>"
 for row in rows:
      currString +="<tr><td>"
-     currString +=str(row['last_name']) 
+     currString +=str(row['zip_code']) 
      currString +="</td><td>"
-     currString +=str(row['first_name']) 
+     currString +=str(row['city']) 
      currString +="</td></tr>"
 currString +="</tbody></table></body></html>"
 
