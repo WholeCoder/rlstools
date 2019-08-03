b92c74ad8c8c32fa4418c36239bdf3957cf87a9e
Ruben Pierich <rpierich@hotmail.com>
2019-08-03 10:52:22 -0400

    Linted test_rls_record.py.

--- test_rls_record.py
+++ test_rls_record.py
@@ -3,34 +3,33 @@ from person_rls_record import Person
 import unittest
 import os
 
+
 class RlsRecordTestCase(unittest.TestCase):
     def setUp(self):
         database_path = "my_test_database.db"
         try:
-            #print("Trying to remove "+database_path)
             os.remove(database_path)
-        except:
-            pass
-            #print("Error deleting "+database_path+" this isn't really an error.")
+        except IOError as err:
+            print("I/O error: {0}".format(err))
         self.dAdapter = SqliteDatabaseAdapter.getTestInstance()
         print(self.dAdapter)
 
     def test_insert_data_into_rls_record_object(self):
         self.dAdapter.createTable("Person")
-        self.dAdapter.addColumn("Person","first_name")
-        self.dAdapter.addColumn("Person","last_name")
+        self.dAdapter.addColumn("Person", "first_name")
+        self.dAdapter.addColumn("Person", "last_name")
 
-        insertDictionary = {"first_name":"Ruben","last_name":"Pierich"}
+        insertDictionary = {"first_name": "Ruben", "last_name": "Pierich"}
         Person().create(insertDictionary)
 
-        insertDictionary = {"first_name":"Shannon","last_name":"Underkoffler"}
+        insertDictionary = {"first_name": "Shannon", "last_name": "Underkoffler"}# noqa
         Person().create(insertDictionary)
 
         rows = Person().findAll()
-        #print("row 1 == " + str(rows[0]))
 
         self.assertTrue(rows[0]['first_name'] == 'Ruben')
         self.assertTrue(rows[0]['last_name'] == 'Pierich')
+
 
 if __name__ == '__main__':
     unittest.main()
