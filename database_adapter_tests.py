b80bdf68f6e194219d021193b8637e29f4eb2500
Ruben Pierich <rpierich@hotmail.com>
2019-08-03 09:55:05 -0400

    Linted database_adapter_tests.py.

--- database_adapter_tests.py
+++ database_adapter_tests.py
@@ -7,30 +7,28 @@ class DatabaseAdapterTestCase(unittest.TestCase):
     def setUp(self):
         database_path = "my_test_database.db"
         try:
-            #print("Trying to remove "+database_path)
             os.remove(database_path)
-        except:
-            pass
-            #print("Error deleting "+database_path+" this isn't really an error.")
+        except IOError as err:
+            print("I/O error: {0}".format(err))
         self.dAdapter = SqliteDatabaseAdapter.getTestInstance()
         if self.dAdapter.doesTableExist("db_versions"):
             self.dAdapter.dropTable("db_versions")
 
-        print("sqllitedatabaseadapter ->>>>>>>>>>>>>>>>>>"+str(SqliteDatabaseAdapter.getTestInstance())
-                )
+        print("sqllitedatabaseadapter ->>>>>>>>>>>>>>>>>>"+str(SqliteDatabaseAdapter.getTestInstance()))# noqa
+
     def test_if_get_next_version_of_database(self):
         nn = self.dAdapter.getNextDatabaseVersionNumber()
         self.assertTrue(nn == "000")
-        self.dAdapter.createNewRecord("db_versions",{"version":"000"})
+        self.dAdapter.createNewRecord("db_versions", {"version": "000"})
         nn = self.dAdapter.getNextDatabaseVersionNumber()
         self.assertTrue(nn == "001")
-        
+
     def test_if_delete_column_brings_back_a_deleted_column(self):
         self.dAdapter.createTable("Person")
-        self.dAdapter.addColumn("Person","zipcode")
+        self.dAdapter.addColumn("Person", "zipcode")
         self.assertTrue(self.dAdapter.doesTableExist("Person"))
         self.dAdapter.dropTable("Person")
-        self.dAdapter.deleteColumnFromTable("Person","zipcode")
+        self.dAdapter.deleteColumnFromTable("Person", "zipcode")
         self.assertFalse(self.dAdapter.doesTableExist("Person"))
 
     def test_add_person_table_without_columns(self):
@@ -45,17 +43,17 @@ class DatabaseAdapterTestCase(unittest.TestCase):
 
     def test_if_can_add_column_to_office_table(self):
         self.dAdapter.createTable("Office")
-        self.dAdapter.addColumn("Office","zipcode")
+        self.dAdapter.addColumn("Office", "zipcode")
         self.assertTrue(self.dAdapter.doesColumnExist("Office", "zipcode"))
     
     def test_if_can_drop_column(self):
         self.dAdapter.createTable("Office")
-        self.dAdapter.addColumn("Office","zipcode")
-        self.dAdapter.deleteColumnFromTable("Office","zipcode")
+        self.dAdapter.addColumn("Office", "zipcode")
+        self.dAdapter.deleteColumnFromTable("Office", "zipcode")
         self.assertFalse(self.dAdapter.doesColumnExist("Office", "zipcode"))
 
 
 if __name__ == '__main__':
-     unittest.main()
+    unittest.main()
 
 
