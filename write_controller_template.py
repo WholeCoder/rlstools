2a22eaea547a22a04fc839053164a9df6fedc6f9
Ruben Pierich <rpierich@hotmail.com>
2019-08-03 10:57:35 -0400

    Linted write_controller_template.py.

--- write_controller_template.py
+++ write_controller_template.py
@@ -7,25 +7,23 @@ print("-----------------------------working---------------")
 
 if not exists:
     print("------------exists")
-    router_file = open("../router.py","w")
+    router_file = open("../router.py", "w")
     router_file.write("router = {}\n")
 else:
     print("-----------doesn't eist")
-    router_file = open("../router.py","a+")
+    router_file = open("../router.py", "a+")
 
 print("--------writing router.py")
 router_file.write("\nrouter['"+sys.argv[2]+"'] = {}")
-router_file.write("\nrouter['"+sys.argv[2]+"']['get_form'] = '"+sys.argv[2]+".get.pyht'\n")
-router_file.write("\nrouter['"+sys.argv[2]+"']['index'] = '"+sys.argv[2]+".pyht'\n")
+router_file.write("\nrouter['"+sys.argv[2]+"']['get_form'] = '"+sys.argv[2]+".get.pyht'\n")# noqa
+router_file.write("\nrouter['"+sys.argv[2]+"']['index'] = '"+sys.argv[2]+".pyht'\n")# noqa
 router_file.close()
-
-
 
 print("Inside of write__controller_template.py. directory == "+sys.argv[0])
 print("app direcotry name = " + sys.argv[1])
 print("scaffold name = " + sys.argv[2])
 
-new_controller = open(sys.argv[1]+"/"+sys.argv[2]+".py",'w')
+new_controller = open(sys.argv[1]+"/"+sys.argv[2]+".py", 'w')
 
 
 classHeader = "import sys\n\n" 
@@ -37,36 +35,35 @@ new_controller.write(classHeader)
 
 count = 3
 
-defHeader =  "    @staticmethod\n"
-defHeader +=  "    def index():\n"
+defHeader = "    @staticmethod\n"
+defHeader += "    def index():\n"
 defHeader += "        print(\"Running template_Output\")\n\n"
 
-defHeader += "        "+sys.argv[2]+"Controller.rows = "+sys.argv[2]+"().findAll()\n"
-defHeader += "        print(\"rows == \"+str("+sys.argv[2]+"Controller.rows))\n\n"
+defHeader += "        "+sys.argv[2]+"Controller.rows = "+sys.argv[2]+"().findAll()\n"# noqa
+defHeader += "        print(\"rows == \"+str("+sys.argv[2]+"Controller.rows))\n\n"# noqa
 
 new_controller.write(defHeader)
 
 defHeader = "    @staticmethod\n"
 defHeader += "    def post():\n"
 new_controller.write(defHeader)
-passText =  "        print('dummy test')\n\n"
+passText = "        print('dummy test')\n\n"
 new_controller.write(passText)
 
 defHeader = "    @staticmethod\n"
 defHeader += "    def get_form():\n"
 new_controller.write(defHeader)
-passText =  "        print('dummy test')\n\n"
+passText = "        print('dummy test')\n\n"
 new_controller.write(passText)
- 
+
 defHeader = "    @staticmethod\n"
 defHeader += "    def put():\n"
 new_controller.write(defHeader)
 new_controller.write(passText)
 
-defHeader =  "    @staticmethod\n"
+defHeader = "    @staticmethod\n"
 defHeader += "    def get():\n"
 new_controller.write(defHeader)
 new_controller.write(passText)
- 
 
 new_controller.close()
