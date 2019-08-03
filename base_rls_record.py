76cf42f284a7363bae060799724ee9be673a3aea
Ruben Pierich <rpierich@hotmail.com>
2019-08-03 09:40:47 -0400

    Linted base_rls_record.py.

--- base_rls_record.py
+++ base_rls_record.py
@@ -1,26 +1,21 @@
-from database_adapter import DatabaseAdapter
-from sqlite_adapter import CursorByName
 from sqlite_adapter import SqliteDatabaseAdapter
+
 
 class BaseRlsRecord:
     def __init__(self):
         self.dAdapter = SqliteDatabaseAdapter.getInstance()
-        print("getting instance in base rls record."+SqliteDatabaseAdapter.getInstance().db_filename)
+        print("getting instance in base rls record."+SqliteDatabaseAdapter.getInstance().db_filename)# noqa
 
-    def typename(self,x):
+    def typename(self, x):
         return type(x).__name__
 
     def get_type_name_of_subclass(self):
         return self.typename(self)
 
-    def create(self,insertDictionary):
+    def create(self, insertDictionary):
         table = self.get_type_name_of_subclass()
-        self.dAdapter.createNewRecord(table,insertDictionary)
+        self.dAdapter.createNewRecord(table, insertDictionary)
 
     def findAll(self):
         rows = self.dAdapter.findAllRecords(self.get_type_name_of_subclass())
-       
-        #for row in rows:
-        #    print("rows2 == "+str(row))
-
         return rows 
