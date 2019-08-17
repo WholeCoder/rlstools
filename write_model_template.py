86b9489b27b1c1bcd5bc089c10c3a33eb6f03e5c
Ruben Pierich <rpierich@hotmail.com>
2019-08-16 22:15:50 -0400

    Added example code for getOneTomanyDicitonary in write_model script.

--- write_model_template.py
+++ write_model_template.py
@@ -8,10 +8,14 @@ print("scaffold name = " + sys.argv[2])
 classHeader = "import sys\n\n"
 classHeader += "sys.path.append('../..')\n\n"
 classHeader += "from base_rls_record import BaseRlsRecord\n\n"
+classHeader += "#  example\n"
+classHeader += "#  from models import Comment\n\n"
 classHeader += "class "+sys.argv[2]+"(BaseRlsRecord):\n"
 with open(sys.argv[1]+"/"+sys.argv[2]+".py", 'w') as new_model:
     new_model.write(classHeader)
     new_model.write("    def getOneToManyDictionary(self):\n")
-    new_model.write("       pass\n")
+    new_model.write("        #  example\n")
+    new_model.write("        #  return {\"Comment\": (Comment.Comment, \"post_primary_key\")}\n")  # noqa
+    new_model.write("        pass\n")
     new_model.write("    pass\n")
     new_model.close()
