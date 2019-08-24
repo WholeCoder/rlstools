b7d29c573c3612e81e5162c60172a98c79ba7d36
Ruben Pierich <rpierich@hotmail.com>
2019-08-23 22:50:59 -0400

    Cleaned up generated code to addhere to PEP8 guidlines.

--- write_model_template.py
+++ write_model_template.py
@@ -7,10 +7,10 @@ print("app direcotry name = " + sys.argv[1])
 print("scaffold name = " + sys.argv[2])
 
 classHeader = "import sys\n\n"
-classHeader += "sys.path.append('../..')\n\n"
 classHeader += "from base_rls_record import BaseRlsRecord\n\n"
+classHeader += "sys.path.append('../..')\n\n"
 classHeader += "#  example\n"
-classHeader += "#  from models import Comment\n\n"
+classHeader += "#  from models import Comment\n\n\n"
 classHeader += "class "+sys.argv[2]+"(BaseRlsRecord):\n"
 with open(os.path.join(sys.argv[1], sys.argv[2]+".py"), 'w') as new_model:
     new_model.write(classHeader)
